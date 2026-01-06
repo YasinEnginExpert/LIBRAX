46966546d62e167b0d7b3d5f445cda220530fc1c
yasin engin <yasinenginoffical@gmail.com>
2026-01-06 15:42:47 +0300

    Rapor PDF eklendi

--- internal/repository/uye.py
+++ internal/repository/uye.py
@@ -4,71 +4,94 @@ class UyeRepository:
 
     def listele(self, keyword=None):
         cur = self.conn.cursor()
-        if keyword:
-            cur.execute(
-                """
-                SELECT uyeid, ad, soyad, email, telefon, toplamborc
-                FROM uye
-                WHERE ad ILIKE %s
-                   OR soyad ILIKE %s
-                   OR email ILIKE %s
-                ORDER BY ad, soyad
-                """,
-                (f"%{keyword}%", f"%{keyword}%", f"%{keyword}%")
-            )
-        else:
-            cur.execute(
-                """
-                SELECT uyeid, ad, soyad, email, telefon, toplamborc
-                FROM uye
-                ORDER BY ad, soyad
-                """
-            )
-        return cur.fetchall()
+        try:
+            if keyword:
+                cur.execute(
+                    """
+                    SELECT uyeid, ad, soyad, email, telefon, toplamborc
+                    FROM uye
+                    WHERE ad ILIKE %s
+                       OR soyad ILIKE %s
+                       OR email ILIKE %s
+                    ORDER BY ad, soyad
+                    """,
+                    (f"%{keyword}%", f"%{keyword}%", f"%{keyword}%")
+                )
+            else:
+                cur.execute(
+                    """
+                    SELECT uyeid, ad, soyad, email, telefon, toplamborc
+                    FROM uye
+                    ORDER BY ad, soyad
+                    """
+                )
+            return cur.fetchall()
+
+        except Exception as e:
+            self.conn.rollback()
+            raise e
 
     def ekle(self, ad, soyad, email, telefon):
         cur = self.conn.cursor()
-        cur.execute(
-            """
-            INSERT INTO uye (ad, soyad, email, telefon, toplamborc)
-            VALUES (%s, %s, %s, %s, 0)
-            """,
-            (ad, soyad, email, telefon)
-        )
-        self.conn.commit()
+        try:
+            cur.execute(
+                """
+                INSERT INTO uye (ad, soyad, email, telefon, toplamborc)
+                VALUES (%s, %s, %s, %s, 0)
+                """,
+                (ad, soyad, email, telefon)
+            )
+            self.conn.commit()
+        except Exception as e:
+            self.conn.rollback()
+            raise e
 
     def guncelle(self, uyeid, ad, soyad, email, telefon):
         cur = self.conn.cursor()
-        cur.execute(
-            """
-            UPDATE uye
-            SET ad=%s, soyad=%s, email=%s, telefon=%s
-            WHERE uyeid=%s
-            """,
-            (ad, soyad, email, telefon, uyeid)
-        )
-        self.conn.commit()
+        try:
+            cur.execute(
+                """
+                UPDATE uye
+                SET ad=%s, soyad=%s, email=%s, telefon=%s
+                WHERE uyeid=%s
+                """,
+                (ad, soyad, email, telefon, uyeid)
+            )
+            self.conn.commit()
+        except Exception as e:
+            self.conn.rollback()
+            raise e
 
     def sil(self, uyeid):
         cur = self.conn.cursor()
-        cur.execute(
-            "DELETE FROM uye WHERE uyeid=%s",
-            (uyeid,)
-        )
-        self.conn.commit()
+        try:
+            cur.execute(
+                "DELETE FROM uye WHERE uyeid=%s",
+                (uyeid,)
+            )
+            self.conn.commit()
+        except Exception as e:
+            self.conn.rollback()
+            raise e
 
     def silinebilir_mi(self, uyeid):
         cur = self.conn.cursor()
-        cur.execute(
-            """
-            SELECT
-                EXISTS (
-                    SELECT 1 FROM odunc
-                    WHERE uyeid = %s
-                    AND teslimtarihi IS NULL
-                ) AS aktif_odunc,
-                (SELECT toplamborc FROM uye WHERE uyeid = %s) > 0 AS borc_var
-            """,
-            (uyeid, uyeid)
-        )
-        return cur.fetchone()
+        try:
+            cur.execute(
+                """
+                SELECT
+                    EXISTS (
+                        SELECT 1 FROM odunc
+                        WHERE uyeid = %s
+                          AND teslimtarihi IS NULL
+                    ) AS aktif_odunc,
+                    (SELECT toplamborc FROM uye WHERE uyeid = %s) > 0 AS
+                    borc_var
+                """,
+                (uyeid, uyeid)
+            )
+            return cur.fetchone()
+
+        except Exception as e:
+            self.conn.rollback()
+            raise e
